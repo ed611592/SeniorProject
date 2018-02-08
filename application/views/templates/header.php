bbfbcfddaa55b2fbe6a9d4f35dbf795db3445001
Emily <ed611592@sju.edu>
2018-02-07 19:50:44 -0500

    Codeigniter Framework
    Added in all the codeigniter files

---
+++ application/views/templates/header.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>CodeIgniter Tutorial </title>
+        <link rel="stylesheet" href = "https://bootswatch.com/4/flatly/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/style.css">
+    </head>
+    <body>
+    	<nav class ="navbar navbar-inverse">
+    		<div class="container">
+    			<div class = "navbar-header">
+    				<a class="navbar-brand" href="<?php echo base_url();?>">ciBlog</a>
+    			</div>
+    			<div id = "navbar">
+    				<ul class = "nav navbar-nav">
+    					<li><a href="<?php echo base_url();?>">Home</a></li>
+    					<li><a href="<?php echo base_url(); ?>about">About</a></li>
+                        <li><a href="<?php echo base_url();?>posts">Blog</a></li>
+    				</ul>
+
+                    <ul class = "nav navbar-nav navbar-right">
+                        <li><a href="<?php echo base_url();?>posts/create">Create Post</a></li>
+    			</div>
+    		</div>
+    	</nav>
+
+    	<div class = "container">
+
+
