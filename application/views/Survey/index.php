c055a8c58a7b5b6194f76992ced7d8f3667f1148
Andrew Less <andrew.less22@gmail.com>
2018-02-26 19:50:25 -0500

    fixed page
    fixed page needs to be formatted

--- application/views/Survey/index.php
+++ application/views/Survey/index.php
@@ -1,16 +1,20 @@
 
 
 <?php
-	//$answer = array('');
+if(!isset($_SESSION['counter'])) {
+    $_SESSION['counter'] = 0;
+}
 
-//	if (isset($answer){
-		//$this -> db -> 
-//	}
+// if button is pressed, increment counter
+if(isset($_POST['button'])) {
+    ++$_SESSION['counter'];
+}    
 
-?>
+// reset counter
+if(isset($_POST['reset'])) {
+    $_SESSION['counter'] = 0;
+}
 
-<?php 
-	//print_r($survey[0]);
 ?>
 
 <?php echo "
@@ -24,14 +28,16 @@
 
  <link href="assets/css/UserCss.css" rel="stylesheet">
 
-<?php foreach ($survey as $question ) : ?>
-	<h3 id = "question"> <?php echo $question['Q_text']; ?></h3>
+<form method="POST">
+	<input type="hidden" name="counter" value="<?php echo $_SESSION['counter']; ?>"/>
+	<h3 id = "question"> <?php echo $survey[$_SESSION['counter']]['Q_text']; ?></h3>
             <div class="list-group">
-              <a href="#" class="list-group-item list-group-item-action" id ="option1" onclick=" return myFunction()"><?php echo $question['option1']; ?></a>
-              <a href="#" class="list-group-item list-group-item-action" id = "option2" onclick=" return myFunction()"><?php echo $question['option2']; ?></a>
-              <a href="#" class="list-group-item list-group-item-action" id = "option3" onclick=" return myFunction()"><?php echo $question['option3']; ?></a>
-              <a href="#" class="list-group-item list-group-item-action" id = "option4" onclick=" return myFunction()"><?php echo $question['option4']; ?></a>
+              <a href="#" class="list-group-item list-group-item-action" id ="option1" onclick=" return myFunction()"><?php echo $survey[$_SESSION['counter']]['option1']; ?></a>
+              <a href="#" class="list-group-item list-group-item-action" id = "option2" onclick=" return myFunction()"><?php echo $survey[$_SESSION['counter']]['option2']; ?></a>
+              <a href="#" class="list-group-item list-group-item-action" id = "option3" onclick=" return myFunction()"><?php echo $survey[$_SESSION['counter']]['option3']; ?></a>
+              <a href="#" class="list-group-item list-group-item-action" id = "option4" onclick=" return myFunction()"><?php echo $survey[$_SESSION['counter']]['option4']; ?></a>
             </div>
-
-<?php endforeach; ?>
+        <input type="submit" name="button" value="Next" />
+        <input type="submit" name="reset" value="Reset" />
+    </form>
 
