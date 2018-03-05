c9167aa2d76c7c793ddc6a045a11a7dd013a5f60
Emily <ed611592@sju.edu>
2018-03-05 16:05:02 -0500

    change value of answer to int

--- application/views/Survey/index.php
+++ application/views/Survey/index.php
@@ -62,22 +62,22 @@ $data = $q->result_array();
 
 
     <div>
-      <input type="radio" name="Student_Answer" id="option1" value="A" />
+      <input type="radio" name="Student_Answer" id="option1" value="1" />
       <label for="option1"><?php echo($data[$_SESSION['counter']]['option1']); ?> </label>
     </div>
 
     <div>
-      <input type="radio" name="Student_Answer" id="option2" value="B" />
+      <input type="radio" name="Student_Answer" id="option2" value="2" />
       <label for="option1"><?php echo($data[$_SESSION['counter']]['option2']); ?> </label>
     </div>
 
     <div>
-      <input type="radio" name="Student_Answer" id="option3" value="C" />
+      <input type="radio" name="Student_Answer" id="option3" value="3" />
       <label for="option1"><?php echo($data[$_SESSION['counter']]['option3']); ?> </label>
     </div>
 
     <div>
-      <input type="radio" name="Student_Answer" id="option4" value="D" />
+      <input type="radio" name="Student_Answer" id="option4" value="4" />
       <label for="option1"><?php echo($data[$_SESSION['counter']]['option4']); ?> </label>
     </div>
 
