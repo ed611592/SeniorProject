259e15103711c2bc5a1b63d4afbedeb475890928
Andrew Less <andrew.less22@gmail.com>
2018-03-07 19:30:37 -0500

    fixed homepage
    no more stats

--- application/views/Survey/index.php
+++ application/views/Survey/index.php
@@ -61,10 +61,6 @@ $data = $q->result_array();
 
       <img id = "chip" src = "<?php echo base_url();?>assets/images/ChipStudent.png">
 
-<<<<<<< HEAD
-
-
-
     <div>
       <input type="radio" name="Student_Answer" id="option1" value="1" />
       <label for="option1"><?php echo($data[$_SESSION['counter']]['option1']); ?> </label>
@@ -84,7 +80,7 @@ $data = $q->result_array();
       <input type="radio" name="Student_Answer" id="option4" value="4" />
       <label for="option1"><?php echo($data[$_SESSION['counter']]['option4']); ?> </label>
     </div>
-=======
+
       <div>
         <input type="radio" name="Student_Answer" id="option1" value="A" />
         <label for="option1"><?php echo($data[$_SESSION['counter']]['option1']); ?> </label>
@@ -104,28 +100,6 @@ $data = $q->result_array();
         <input type="radio" name="Student_Answer" id="option4" value="D" />
         <label for="option1"><?php echo($data[$_SESSION['counter']]['option4']); ?> </label>
       </div>
-
-      <div>
-        <input type="radio" name="Student_Answer" id="option1" value="1" />
-        <label for="option1"><?php echo($data[$_SESSION['counter']]['option1']); ?> </label>
-      </div>
-
-      <div>
-        <input type="radio" name="Student_Answer" id="option2" value="2" />
-        <label for="option1"><?php echo($data[$_SESSION['counter']]['option2']); ?> </label>
-      </div>
-
-      <div>
-        <input type="radio" name="Student_Answer" id="option3" value="3" />
-        <label for="option1"><?php echo($data[$_SESSION['counter']]['option3']); ?> </label>
-      </div>
-
-      <div>
-        <input type="radio" name="Student_Answer" id="option4" value="4" />
-        <label for="option1"><?php echo($data[$_SESSION['counter']]['option4']); ?> </label>
-      </div>
->>>>>>> 07f6e0e816d5d15572db0342db563305b80fe46c
-
 
       <input type="hidden" class = "form-control" name = "Q_ID" value= "<?php echo($data[$_SESSION['counter']]['Q_ID']); ?>" />
 
