cdcc6b4f2a7baa99eadecf79cf6ddccf5a45aa23
Emily <ed611592@sju.edu>
2018-03-08 13:41:15 -0500

    test.php

--- application/views/pages/test.php
+++ application/views/pages/test.php
@@ -5,36 +5,39 @@ echo 'hi';
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
-//this library works
-use MathPHP\Algebra;
-
-
-// Greatest common divisor (GCD)
-
-$gcd = Algebra::gcd(8, 12);
 
 
 
 use MCordingley\Regression\Observations;
+use MCordingley\Regression\Observation;
+use MCordingley\Regression\Algorithm\LeastSquares;
 
 $observations = new Observations;
+//i think this is answers to the questions
+$arraytest = array(1,2);
+//y value aka grade in class?
+$outcometest = 90.0;
+
+$obs1 = new Observation($arraytest, $outcometest);
+$obs2 = new Observation($arraytest, $outcometest);
+$obs3 = new Observation($arraytest, $outcometest);
+$obs4 = new Observation($arraytest, $outcometest);
+
 
 $data = array
   (
-  array("Volvo",22,18),
-  array("BMW",15,13),
-  array("Saab",5,2),
-  array("Land Rover",17,15)
+  $obs1, $obs2, $obs3, $obs4
   );
 
 //so it will go through the array data, and loop through each array and refer to it as $datum
 foreach ($data as $datum) {
     // Note addition of a constant for the first feature.
     //$datum-> features means we have to get the feature of that specific array?
-    $observations->add(array_merge([1.0], $datum->features), $datum->outcome);
+    $observations->add(array_merge([1.0], $datum->getFeatures()), $datum->getOutcome());
 }
 
-use MCordingley\Regression\Algorithm\LeastSquares;
+
 $algorithm = new LeastSquares;
+$coefficients = $algorithm->regress($observations);
 
 ?>
