e75a5ed8f138f5f4eebe1e37d5a1c776b4d37a0f
Brandon <bj627334@sju.edu>
2018-03-20 17:43:26 -0400

    css/html work

--- application/views/Survey/index.php
+++ application/views/Survey/index.php
@@ -61,25 +61,26 @@ $data = $q->result_array();
 
       <img id = "chip" src = "<?php echo base_url();?>assets/images/ChipStudent.png">
 
+    <div id= "answer">
+      <div>
+        <input type="radio" name="Student_Answer" id="option1" value="1" />
+        <label for="option1"><?php echo($data[$_SESSION['counter']]['option1']); ?> </label>
+      </div>
 
-    <div>
-      <input type="radio" name="Student_Answer" id="option1" value="1" />
-      <label for="option1"><?php echo($data[$_SESSION['counter']]['option1']); ?> </label>
-    </div>
+      <div>
+        <input type="radio" name="Student_Answer" id="option2" value="2" />
+        <label for="option1"><?php echo($data[$_SESSION['counter']]['option2']); ?> </label>
+      </div>
 
-    <div>
-      <input type="radio" name="Student_Answer" id="option2" value="2" />
-      <label for="option1"><?php echo($data[$_SESSION['counter']]['option2']); ?> </label>
-    </div>
+      <div>
+        <input type="radio" name="Student_Answer" id="option3" value="3" />
+        <label for="option1"><?php echo($data[$_SESSION['counter']]['option3']); ?> </label>
+      </div>
 
-    <div>
-      <input type="radio" name="Student_Answer" id="option3" value="3" />
-      <label for="option1"><?php echo($data[$_SESSION['counter']]['option3']); ?> </label>
-    </div>
-
-    <div>
-      <input type="radio" name="Student_Answer" id="option4" value="4" />
-      <label for="option1"><?php echo($data[$_SESSION['counter']]['option4']); ?> </label>
+      <div>
+        <input type="radio" name="Student_Answer" id="option4" value="4" />
+        <label for="option1"><?php echo($data[$_SESSION['counter']]['option4']); ?> </label>
+      </div>
     </div>
 
 
