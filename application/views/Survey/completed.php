bfd32eb742ae53dbd3b24f1f5572ca4d286dfa9c
lessismore12 <32404927+lessismore12@users.noreply.github.com>
2018-03-21 10:15:17 -0400

    completed survey page
    made a cute little completed page. May need more idk

--- application/views/Survey/completed.php
+++ application/views/Survey/completed.php
@@ -1,3 +1,10 @@
-<?php 
-echo 'hi'; 
-?>
+<style>
+img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
+
+<img SRC="<?php echo base_url();?>assets/completed.gif">
+<h2>Please logout and wait for everyone else to be done!!!</h2>
