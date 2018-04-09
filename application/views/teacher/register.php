eb08580184693d2b2c39d65f6c18f424fdd665dc
Brandon <bj627334@sju.edu>
2018-04-09 14:56:36 -0400

    Fixed sizing issues

--- application/views/teacher/register.php
+++ application/views/teacher/register.php
@@ -14,30 +14,31 @@
 <?php echo form_open('teacher/register'); ?>
 
  <body id= "teacher">
-	<div class = "row">
-		<div class = 'col-md-4 col-md-offset-4'>
-			<div class = "form-group">
-				<label>Name</label>
-				<input type = "text" class = "form-control" name = "name" placeholder = "Name">
-			</div>
+ 	<center>
+		<div class = "row">
+			<div class = 'col-md-4 col-md-offset-4'>
+				<div class = "form-group">
+					<label>Name</label>
+					<input type = "text" class = "form-control" name = "name" placeholder = "Name">
+				</div>
 
-			<div class = "form-group">
-				<label>Username</label>
-				<input type = "text" class = "form-control" name = "username" placeholder = "Username">
-			</div>
+				<div class = "form-group">
+					<label>Username</label>
+					<input type = "text" class = "form-control" name = "username" placeholder = "Username">
+				</div>
 
-			<div class = "form-group">
-				<label>Password</label>
-				<input type = "password" class = "form-control" name = "password" placeholder = "Password">
-			</div>
+				<div class = "form-group">
+					<label>Password</label>
+					<input type = "password" class = "form-control" name = "password" placeholder = "Password">
+				</div>
 
-			<div class = "form-group">
-				<label>Confirm Password</label>
-				<input type = "password" class = "form-control" name = "password2" placeholder = "Confirm Password">
+				<div class = "form-group">
+					<label>Confirm Password</label>
+					<input type = "password" class = "form-control" name = "password2" placeholder = "Confirm Password">
+				</div>
 			</div>
 		</div>
-	</div>
-	<center>
+		
 		<button type = "submit" class = "btn btn-primary btn-block">Submit</button>
 	</center>
 
